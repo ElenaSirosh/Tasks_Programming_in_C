98a6783ecf6c04afcc3912ec2a053b0735042a8b
Elena Sirosh <elena.verenich.sirosh@gmail.com>
2024-03-02 01:48:04 +0100

    chapter 9, exercise 5

--- chapter9/ex5.c
+++ chapter9/ex5.c
@@ -1,3 +1,12 @@
+/*
+ * chapter 9, exercise 5
+ * Write a function called clockKeeper that takes as its argument a dateAndTime
+ * structure as defined in this chapter.The function should call the timeUpdate function,
+ * and if the time reaches midnight, the function should call the dateUpdate
+ * function to switch over to the next day. Have the function return the updated
+ * dateAndTime structure.
+ */
+
 #include <stdio.h>
 
 // Structure to represent date
